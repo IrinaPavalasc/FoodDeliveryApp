03c02f2726600e28d69b1f3046833b9e31ed6831
Irina Pavalasc <irinapavalasc@yahoo.ro>
2023-01-06 21:41:48 +0200

    First commit

---
+++ src/main/java/com/fmiunibuc/FoodDeliveryApp/FoodDeliveryAppApplication.java
@@ -0,0 +1,13 @@
+package com.fmiunibuc.FoodDeliveryApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliveryAppApplication.class, args);
+	}
+
+}
